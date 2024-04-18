d62a8fbbee2aa7fe0961d01c8717c6332ac70e1b
vigenpro-lab <vigen.k2007@mail.ru>
2024-04-18 12:56:20 +0300

    Initial commit

--- widget.py
+++ widget.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
-"""функция которая шифрует и номер карты и номер счета"""
-
 
 def mask_info(input_string: str) -> str:
+    """функция которая шифрует и номер карты и номер счета"""
     parts = input_string.split()
     if len(parts) == 2:
         return f"{parts[0]} {parts[1][:6]}******{parts[1][-4:]}"
@@ -13,9 +12,7 @@ def mask_info(input_string: str) -> str:
         return f"Счет **{input_string[-4:]}"
 
 
-"""функция которая возвращет дату"""
-
-
 def convert_date(data: str) -> str:
+    """функция которая возвращет дату"""
     beta = datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%f')
     return beta.strftime('%d.%m.%Y')
