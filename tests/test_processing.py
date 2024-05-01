1a7966940e543eedd48f9f656905f2bbd96c47a3
vigenpro-lab <vigen.k2007@mail.ru>
2024-05-02 00:25:19 +0300

    Initial commit

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -6,10 +6,10 @@ from src.processing import filter_state, sort_date
 @pytest.fixture
 def data() -> List[Dict[str, str]]:
     return [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {'id': '41428829', 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': '939719570', 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': '594226727', 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': '615064591', 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
 
 
@@ -22,6 +22,6 @@ def test_filter_state(data: List[Dict[str, str]], x: str, total: int) -> None:
 
 
 @pytest.mark.parametrize("x, total", [("desc", True), ("asc", False)])
-def test_sort_date(data: List[Dict[str, str]], x: str, total: int) -> None:
-    result = sort_date(data, order=x)
+def test_sort_date(data: List[Dict[str, str]], x: str, total: bool) -> None:
+    result = sort_date(data)
     assert len(result) == 4
