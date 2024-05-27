dd495636a693b3cbfc7a1ae38ebd333bb46b4328
vigenpro-lab <vigen.k2007@mail.ru>
2024-05-27 16:14:09 +0300

    Initial commit

--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,9 @@ API_KEY = os.getenv("api_keys")
 
 
 def read_json_file(fp: str) -> List[Dict[str, Union[str, float]]]:
+    """Читает JSON файл и возвращает данные в виде списка словарей"""
     try:
-        with open(fp, 'r') as file:
+        with open(fp, "r") as file:
             data = json.load(file)
             if isinstance(data, list):
                 return data
@@ -23,6 +24,7 @@ def read_json_file(fp: str) -> List[Dict[str, Union[str, float]]]:
 
 
 def transactions_rub_to_usd(curren: str) -> Union[float, Any]:
+    """Получает текущий курс валюты по отношению к рублю (RUB) с использованием внешнего API"""
     url = f"https://v6.exchangerate-api.com/v6/92038b02fd9e7e1c6b8b993d/latest/USD={curren}"
     head = {"apikey": API_KEY}
     try:
@@ -31,25 +33,25 @@ def transactions_rub_to_usd(curren: str) -> Union[float, Any]:
         response_data = response.json()
         return response_data["rates"]["RUB"]
     except requests.exceptions.RequestException as ef:
+        print(f"Ошибка при получении обменного курса: {ef}")
         return 1.0
 
 
-def sum_amount(beta: Dict[str, Any]) -> float:
+def sum_amount(transaction: Dict[str, Any]) -> float:
+    """Конвертирует сумму транзакции в рубли, если валюта не является USD или EUR"""
     """Возвращает сумму транзакции в рублях."""
-    # answer = 0.0
-    sum = beta.get("operationAmount", {})
-    # code = sum.get("currency", {}).get("code", "")
-    amount = float(sum.get("amount", 0.0))
-    # if code in ["USD", "EUR"]:
-    #     rate_rub = transactions_rub_to_usd(code)
-    #     answer += amount * rate_rub
-    # elif code == "RUB":
-    #     answer += amount
+    operation_amount = transaction.get("operationAmount", {})
+    currency_code = operation_amount.get("currency", {}).get("code", "")
+    amount = float(operation_amount.get("amount", 0.0))
+
+    if currency_code not in ["USD", "EUR"]:
+        rate_rub = transactions_rub_to_usd(currency_code)
+        amount *= rate_rub
 
     return amount
 
 
-beta = {
+data = {
     "id": 441945886,
     "state": "EXECUTED",
     "date": "2019-08-26T10:50:58.294041",
@@ -59,4 +61,4 @@ beta = {
     "to": "Счет 64686473678894779589",
 }
 
-print(sum_amount(beta))
+print(sum_amount(data))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,12 +6,12 @@ from src.utils import read_json_file, transactions_rub_to_usd, sum_amount
 class TestMyModule(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open, read_data='[{"name": "Test", "amount": 10.0}]')
-    def test_read_json_file(self, mock_file):
+    def test_read_json_file(self, mock_file: MagicMock) -> None:
         result = read_json_file("test.json")
         self.assertEqual(result, [{"name": "Test", "amount": 10.0}])
 
     @patch("requests.get")
-    def test_transactions_rub_to_usd(self, mock_get):
+    def test_transactions_rub_to_usd(self, mock_get: MagicMock) -> None:
         mock_response = MagicMock()
         mock_response.raise_for_status.return_value = None
         mock_response.json.return_value = {"rates": {"RUB": 75.0}}
@@ -20,7 +20,7 @@ class TestMyModule(unittest.TestCase):
         result = transactions_rub_to_usd("EUR")
         self.assertEqual(result, 75.0)
 
-    def test_sum_amount(self):
+    def test_sum_amount(self) -> None:
         beta = {"operationAmount": {"currency": {"code": "RUB"}, "amount": 100.0}}
         result = sum_amount(beta)
         self.assertEqual(result, 100.0)
